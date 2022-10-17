d7f9519823504a0d1e9f4915e25d0ed27338c5c6
aravindan <aravindan.acct@gmail.com>
2022-10-17 15:21:22 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Credential Stuffing Test
+
+Installation:
+
+$ source bin/activate
+$ pip3 install -r requirements.txt
+
+The script can send auth request with either form authentication or json api request
+
+
---
+++ cred_tester.py
@@ -0,0 +1,33 @@
+import requests
+import json
+import argparse
+
+# Create the parser
+parser = argparse.ArgumentParser()
+# Add an argument
+parser.add_argument('--auth_type', type=str, required=True, help="Type 'form' or 'json'")
+parser.add_argument('--url', type=str, required=True, help="Full URL of the login page")
+# Parse the argument
+args = parser.parse_args()
+
+
+with open("input_file", "r") as f:
+    credentials = f.read()
+
+
+#login_url = "https://waf.barracuda.com/cgi-mod/index.cgi"
+print(args.auth_type)
+
+login_url = args.url
+print(login_url)
+for cred in json.loads(credentials):
+    if args.auth_type == "form":
+        login_request = requests.post(login_url, data=cred)
+        print(login_request.status_code)
+    elif args.auth_type == "json":
+        headers = {"Content-Type": "application/json"}
+        login_request = requests.post(login_url, data = json.dumps(cred), headers=headers)
+        print(login_request.status_code)
+    else:
+        print("invalid input")
+        exit
